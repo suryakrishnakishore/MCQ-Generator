bab6518f322429434a1982edc2c4ae611ab6a583
suryakrishnakishore <suryakrishnakishore@gmail.com>
2026-03-10 16:02:27 +0530

    Made the actual model and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='S.S.K.Kishore',
+    author_email='suryakrishnakishore@gmail.com',
+    packages=find_packages()
+)
